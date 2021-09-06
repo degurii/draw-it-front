ceb12aa0bbaf45610555f627f7d208ba22fd5631
degurii <sansam709@gmail.com>
2021-09-07 01:13:13 +0900

    feat: [#18] useStomp에서 log 부분 제거
    - console.log() 제거
    
    - stompjs debug 메서드가 디폴트로 콘솔에 출력하도록 되어있는 걸 제거(빈 함수로 대체)

--- src/hooks/useStomp.js
+++ src/hooks/useStomp.js
@@ -6,6 +6,7 @@ const connectPromise = url =>
     new Promise((resolve, reject) => {
         const sock = SockJS(url);
         const stompClient = Stomp.over(sock);
+        stompClient.debug = () => {};
         const onConnect = () => resolve(stompClient);
         const onError = error => reject(new Error(error));
         stompClient.connect({}, onConnect, onError);
@@ -15,16 +16,14 @@ export default () => {
     const [stomp, setStomp] = useState(null);
     const [success, setSuccess] = useState(false);
     const [connecting, setConnecting] = useState(false);
-    const [error, setError] = useState(false);
+    const [error, setError] = useState(null);
 
     const publish = msg => {
         if (!stomp) return;
-        console.log('publish');
         stomp.send(process.env.REACT_APP_STOMP_PUBLISH_ENDPOINT, {}, msg);
     };
     const subscribe = callback => {
         if (!stomp) return;
-        console.log('subscribe');
         stomp.subscribe(
             process.env.REACT_APP_STOMP_SUBSCRIBE_ENDPOINT,
             callback,
@@ -37,12 +36,11 @@ export default () => {
             const stompClient = await connectPromise(
                 process.env.REACT_APP_WEBSOCKET_URL,
             );
-            stompClient.debug = () => {};
             setStomp(stompClient);
             setSuccess(true);
             setConnecting(false);
         } catch (e) {
-            setError(true);
+            setError(e);
         }
     };
 
